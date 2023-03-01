f0f2618590aa476b13a61693f68badaf87120761
miroslawszyper <miroslawszyper@gmail.com>
2023-03-01 10:57:42 +0100

    modyfikacja pliku

--- dIinz_2_K14_Inf/1_podstawy.php
+++ dIinz_2_K14_Inf/1_podstawy.php
@@ -1,3 +1,15 @@
 <?php 
-  echo "wsb";
+  $firstName = "Janusz";
+  $lastName = "Nowak";
+  echo "Imię i nazwisko: $firstName $lastName<br>";
+  echo 'Imię i nazwisko: $firstName $lastName<br>';
+  
+  //heredoc
+  echo <<< DATA
+    <hr>
+    Imię: $firstName<br>
+    Nazwisko: $lastName<br>
+DATA;
+
+
 ?>
